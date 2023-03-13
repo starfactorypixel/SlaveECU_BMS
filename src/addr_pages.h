a70827a51e5f9f1dcc2f9117a7297b21fc38d316
GrKoR <dr.g.kornev@gmail.com>
2023-03-13 19:10:38 +0400

    unused files was deleted

--- src/addr_pages.h
+++ src/addr_pages.h
@@ -1,13 +1,13 @@
 /*
  * addr_pages.h
  *
- *  Created on: 20 сент. 2019 г.
+ *  Created on: 20 пїЅпїЅпїЅпїЅ. 2019 пїЅ.
  *      Author: dima
  */
 
 #ifndef ADDR_PAGES_H_
 #define ADDR_PAGES_H_
 
-
+// TODO: used nowhere. Should we delete it?
 
 #endif /* ADDR_PAGES_H_ */
