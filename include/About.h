c533e435a5626e2d58eb7479258bfd633eaf5a56
GrKoR <dr.g.kornev@gmail.com>
2024-05-31 23:42:19 +0300

    fix: board_ver++

--- include/About.h
+++ include/About.h
@@ -5,7 +5,7 @@ namespace About
 	static constexpr char name[] = "BMSReaderECU";
 	static constexpr char desc[] = "BMS interface board for Pixel project";
 	static constexpr char board_type = Consts::BOARD_TYPE_BMS1;		// 5 bits
-	static constexpr char board_ver = 1;		// 3 bits
+	static constexpr char board_ver = 2;		// 3 bits
 	static constexpr char soft_ver = 1;			// 6 bits
 	static constexpr char can_ver = 1;			// 2 bits
 	static constexpr char git[] = "https://github.com/starfactorypixel/SlaveECU_BMS";
